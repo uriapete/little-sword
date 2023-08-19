3e223c57c453cf7ab6b643096cba46f1bbcb05de
uriP <uriarteangelo@gmail.com>
2023-08-19 13:20:31 -0700

    Player.cs - add exported var 'Speed'

--- Player.cs
+++ Player.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Player : Area2D
 {
+	[Export]
+	public int Speed { get; set; } = 10;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
